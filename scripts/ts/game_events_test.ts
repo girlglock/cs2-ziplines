2d210937d850d9f06c95c2905be4d619904aa04b
Deana <grcjn.b.rcki@gmail.com>
2025-09-30 16:08:36 +0200

    add blips example

--- scripts/ts/game_events_test.ts
+++ scripts/ts/game_events_test.ts
@@ -1,8 +1,10 @@
 import { Instance as css } from "cs_script/point_script";
 import { EventListeners } from '../eventlisteners/eventlisteners';
+import { Blips } from "../blips/blips";
 import { HitGroup } from "../enums/hitgroups";
 
 EventListeners.RegisterAll();
+const blips = new Blips();
 
 //kick the host if they try to change a nono cvar
 EventListeners.OnServerCvar((data) => {
@@ -22,9 +24,32 @@ EventListeners.OnServerCvar((data) => {
     }
 });
 
-//print hurt data
+//print hurt data using blips
 EventListeners.OnPlayerHurt((data) => {
-    const attackerCtrl = css.GetPlayerController(data.attacker);
-    const victimCtrl = css.GetPlayerController(data.userid);
-    css.Msg(`${attackerCtrl?.GetPlayerName()} -> ${data.weapon} ->  ${victimCtrl?.GetPlayerName()} (${HitGroup[data.hitgroup]}) | DMG: HP ${data.dmg_health} A ${data.dmg_armor} | LEFT: HP ${data.health} A ${data.armor}`);
+    const attackerName = css.GetPlayerController(data.attacker)?.GetPlayerName();
+    const victimName = css.GetPlayerController(data.userid)?.GetPlayerName();
+    const limb = HitGroup[data.hitgroup];
+
+    const msg = `{red}${attackerName}{white} 
+              {red}[${data.weapon}]{white} 
+              {blue}${victimName}{white} (${limb})
+              | {yellow}DMG - HP:${data.dmg_health}  A:${data.dmg_armor}{white}
+              | {green}LEFT - HP:${data.health}  A:${data.armor}`
+        .trim()
+        .replace(/\s+/g, ' ');
+
+    blips.print(msg);
 });
+
+function think() {
+    const gameTime = css.GetGameTime();
+    blips.update(gameTime);
+    css.SetNextThink(gameTime + 0.015625);
+}
+
+blips.print("{white}ⓘ {yellow}Welcome to Movement Hub V2!");
+blips.print("{white}ⓘ {yellow}Welcome to Movement Hub V2!");
+blips.print("{white}ⓘ {yellow}Welcome to Movement Hub V2!");
+
+css.SetNextThink(css.GetGameTime() + 0.015625);
+css.SetThink(() => think());
